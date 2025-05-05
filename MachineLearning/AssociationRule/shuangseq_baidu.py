1c966cffe75c23ad2f6a39880992f1e59702dc1c
Xianyi Ye <yexianyi@hotmail.com>
2025-05-05 18:46:35 +0800

    Update shuangseq_baidu.py

--- MachineLearning/AssociationRule/shuangseq_baidu.py
+++ MachineLearning/AssociationRule/shuangseq_baidu.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import sequential
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+
 
 
 # 读取数据
